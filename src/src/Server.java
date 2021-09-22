8189c1ef2231a8672411e6a35863072197527ba4
James Rivett <jrivett@students.kennesaw.edu>
2021-09-22 18:20:25 -0400

    Router Host now mapped to localhost

--- src/src/Server.java
+++ src/src/Server.java
@@ -13,7 +13,7 @@ public class Server {
         BufferedReader in = null; // for reading form ServerRouter
 		InetAddress addr = InetAddress.getLocalHost();
 		String host = addr.getHostAddress(); // Server machine's IP			
-		String routerName = "j263-08.cse1.spsu.edu"; // ServerRouter host name
+		String routerName = "127.0.0.1"; // ServerRouter host name
 		int SockNum = 5555; // port number
 			
 		// Tries to connect to the ServerRouter
